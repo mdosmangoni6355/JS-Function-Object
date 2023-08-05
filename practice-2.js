d4d1010d28f4367020c824867fc7c3e0553e0794
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-06 00:48:48 +0600

    practice-2.js added

---
+++ practice-2.js
@@ -0,0 +1,11 @@
+function make_avg(num1, num2, num3){
+    const sum = num1 + num2 + num3;
+    const average = sum / 3;
+    return average;
+}
+const avg1 = make_avg(10, 20, 30);
+console.log(avg1);
+const avg2 = make_avg(40, 50, 60);
+console.log(avg2);
+const avg3 = make_avg(70, 80, 90);
+console.log(avg3);
